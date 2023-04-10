7d30a9a88cadffe5dd55727163bd8ae5337adf64
sabrina <sabrinachhuoy@outlook.com>
2023-04-10 10:58:57 -0700

    fixes to error

--- week-1/script.js
+++ week-1/script.js
@@ -1,11 +1,11 @@
-// let firstName = "Sabrina";
-// let lastName = "Chhuoy";
-// let yearsOfStudy = 1;
-// let  goal = 'become a developer';
+let firstName = "Sabrina";
+let lastName = "Chhuoy";
+let yearsOfStudy = 1;
+let  goal = 'become a developer';
 
-// //("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
+//("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
 
 
-// let introduction = 'My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year and plan to ${goal} after completing my courses.'; 
+let introduction = `My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year and plan to ${goal} after completing my courses.`; 
 
-// document.getElementById("introduction").innerHTML = introduction;
+document.getElementById("introduction").innerHTML = introduction;
